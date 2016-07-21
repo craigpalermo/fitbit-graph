39127b2f71e0a702553285a39fdcaf50ea7cbc63
Craig Palermo <craig.palermo@opmbx.org>
2016-07-21 21:01:54 +0000

    Added dropdown to choose which field is shown in graph

--- public/javascripts/components/index/indexCtrl.js
+++ public/javascripts/components/index/indexCtrl.js
@@ -2,6 +2,25 @@ angular.module('fitbit-app').controller('indexCtrl', [
   function () {
     const self = this;
 
+    self.chartFields = [
+      { field: 'caloriesBurned', displayName: 'Caloried Burned' },
+      { field: 'steps', displayName: 'Steps' },
+      { field: 'distance', displayName: 'Distance' },
+      { field: 'floors', displayName: 'Floors' },
+    ];
+
+    self.selectedField = self.chartFields[0].field;
+
+    /**
+     * Sets the selected field and updates chartData to use the values for the given field
+     * @param data
+     * @param fieldName
+     */
+    self.chartDataForField = function chartDataForField(data, fieldName) {
+      self.selectedField = fieldName;
+      self.chartData = [data.map(x => x[fieldName])];
+    }
+
     self.onUpload = function onUpload (files) {
       // TODO progress spinner or something
     };
@@ -15,8 +34,9 @@ angular.module('fitbit-app').controller('indexCtrl', [
         // Extract dates to use as labels
         self.chartLabels = res.data.map(x => x.date);
 
-        // Extract data to display
-        self.chartData = [res.data.map(x => x.caloriesBurned)];
+        // Extract data to display, show caloriesBurned by default
+        self.responseData = res.data;
+        self.chartDataForField(res.data, self.selectedField);
       }
     };
 
